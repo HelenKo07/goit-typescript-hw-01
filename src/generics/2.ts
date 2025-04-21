b01eaf12b2eb8ba4e520ac3fb773b6287cd3abaf
Alena_Korma <alena.gorova93@gmail.com>
2025-04-22 01:49:39 +0300

    commit message

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -5,7 +5,7 @@ type AllType = {
      weight: number
     }
     
-    function compare (top: AllType, bottom: AllType): Pick<AllType, 'name' | 'position' | 'color' | 'weight'> {
+    function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType  {
      return {
       name: top.name,
       color: top.color,
